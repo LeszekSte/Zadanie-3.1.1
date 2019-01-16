0fbe088062f4227ea6982d5dc87b5b0273c0ee08
LeszekS <leszekstepien@o2.pl>
2019-01-16 09:00:37 +0100

    Wersja 1.1

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        OpisPrzedmiotu drukarka1 = new OpisPrzedmiotu()"Drukarka Minolta", "Laserowa",
+//                true, 120, 60, 50);
+//
+//        OpisPrzedmiotu drukarka2 = new OpisPrzedmiotu("Drukarka HP", "Laserowa",
+//                false, 24, 35, 30);
+
+        OpisPrzedmiotu drukarka1 = new OpisPrzedmiotu();
+        drukarka1.nazwa = "Minolta";
+        drukarka1.typ = "Laserowa";
+        drukarka1.prinColor = true;
+        drukarka1.dimensionsHeigh = 120;
+        drukarka1.dimensionsWidth = 60;
+        drukarka1.dimensionsDepth = 50;
+
+        OpisPrzedmiotu drukarka2 = new OpisPrzedmiotu();
+        drukarka2.nazwa = "HP";
+        drukarka2.typ = "Laserowa";
+        drukarka2.prinColor = false;
+        drukarka2.dimensionsHeigh = 24;
+        drukarka2.dimensionsWidth = 35;
+        drukarka2.dimensionsDepth = 30;
+
+
+        int nrDrukarki = 0;
+        System.out.println("Drukarki w pokoju 123");
+        System.out.println("Drukarka nr " + ++nrDrukarki);
+        drukarka1.wydruk();
+        System.out.println("Drukarka nr " + ++nrDrukarki);
+        drukarka2.wydruk();
+
+    }
+}
---
+++ src/OpisPrzedmiotu.java
@@ -0,0 +1,32 @@
+public class OpisPrzedmiotu {
+    String nazwa;
+    String typ;
+    boolean prinColor;
+    double dimensionsHeigh;
+    double dimensionsWidth;
+    double dimensionsDepth;
+
+//    public OpisPrzedmiotu(String nazwa, String typ, boolean prinColor, double dimensionsHeigh,
+//                          double dimensionsWidth, double dimensionsDepth) {
+//        this.nazwa = nazwa;
+//        this.typ = typ;
+//        this.prinColor = prinColor;
+//        this.dimensionsHeigh = dimensionsHeigh;
+//        this.dimensionsWidth = dimensionsWidth;
+//        this.dimensionsDepth = dimensionsDepth;
+//    }
+
+
+
+    void wydruk (){
+        int kolejnosc = 0;
+        System.out.printf("%d. Nazwa        %s\n" ,++kolejnosc,nazwa);
+        System.out.printf("%d. Typ          %s\n" ,++kolejnosc,typ);
+        System.out.printf("%d. Druk Kolor   %s\n" ,++kolejnosc,prinColor);
+        System.out.printf("%d. Wysokosc     %.0f cm\n" ,++kolejnosc,dimensionsHeigh);
+        System.out.printf("%d. Szerokosc    %.0f cm\n" ,++kolejnosc,dimensionsWidth);
+        System.out.printf("%d. Glebokosc    %.0f cm\n\n" ,++kolejnosc,dimensionsDepth);
+
+    }
+
+}
